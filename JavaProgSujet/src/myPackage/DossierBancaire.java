41b5940988ea4581935d6b1096fd25bce58179e1
CONANEC <CONANEC@gmail.com>
2023-01-09 14:34:56 +0100

    modif yohann

--- JavaProgSujet/src/myPackage/DossierBancaire.java
+++ JavaProgSujet/src/myPackage/DossierBancaire.java
@@ -7,7 +7,7 @@ public class DossierBancaire {
     {
     	m_solde=0;
     }
-
+    //commentaire yohann
     public void deposer(double value) {m_solde+=value;}
     public double get_solde() {return m_solde;}
     public void remunerer() {}
